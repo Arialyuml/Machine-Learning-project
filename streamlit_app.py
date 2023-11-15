77217719b880f936ecf46632f77a629c4b86c754
Arialyuml <104297238+Arialyuml@users.noreply.github.com>
2023-11-15 11:52:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,14 +8,14 @@ import pandas as pd
 dn_model = load_model('model_tf.h5')
 
 # Define class labels
-class_labels =['0', '1', '10', '100', '101', '11', '12', '13', '14', '15', '16', '17', '18', '19', '2', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '3', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '4', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '5', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '6', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '7', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '8', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '9', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99']
+class_labels = ['0', '1', '10', '100', '101', '11', '12', '13', '14', '15', '16', '17', '18', '19', '2', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '3', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '4', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '5', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '6', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '7', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '8', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '9', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99']
+
 st.title("Image Classifier")
 
 # Upload the image
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 if uploaded_file is not None:
     img = image.load_img(uploaded_file, target_size=(150, 150))
-    st.write('Please upload an image for classification')
     st.image(img, caption='Uploaded Image.', use_column_width=True)
 
     if st.button('Predict'):
@@ -24,4 +24,4 @@ if uploaded_file is not None:
         img = np.expand_dims(img, axis=0)
         pred = dn_model.predict(img)  # make prediction using DenseNet model
 
-        st.write(f"Prediction: {class_labels[np.argmax(predictions[0])]}")
+        st.write(f"Prediction: {class_labels[np.argmax(pred[0])]}")  # Display prediction
